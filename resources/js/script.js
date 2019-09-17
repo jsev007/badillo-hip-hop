9073866ec5b4a75a0cf3f311130edfe8c2a2220f
Jorge Sevilla <jorgesevilla7@gmail.com>
2019-09-16 23:37:38 -0700

    nav bar navigation edits

--- resources/js/script.js
+++ resources/js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     });
     
     
-    /* Navigation scroll */
+    /* navigation scroll */
     $(function() {
       $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
